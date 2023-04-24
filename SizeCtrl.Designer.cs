c5efdd17549e72ddea6990392ee2503b4e14ba8c
whpark <wh.park@apsystems.co.kr>
2023-04-24 17:39:42 +0900

    dock -> auto size reset

--- SizeCtrl.Designer.cs
+++ SizeCtrl.Designer.cs
@@ -34,13 +34,13 @@ namespace WinFormsApp
             // 
             // lblSize
             // 
-            this.lblSize.AutoSize = true;
             this.lblSize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblSize.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lblSize.Font = new System.Drawing.Font("Cascadia Code", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.lblSize.Location = new System.Drawing.Point(0, 0);
             this.lblSize.Margin = new System.Windows.Forms.Padding(4);
             this.lblSize.Name = "lblSize";
-            this.lblSize.Size = new System.Drawing.Size(133, 43);
+            this.lblSize.Size = new System.Drawing.Size(228, 176);
             this.lblSize.TabIndex = 0;
             this.lblSize.Text = "(0, 0)";
             this.lblSize.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -54,7 +54,6 @@ namespace WinFormsApp
             this.Size = new System.Drawing.Size(228, 176);
             this.SizeChanged += new System.EventHandler(this.SizeCtrl_SizeChanged);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
